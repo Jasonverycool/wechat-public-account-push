1b54c007d7130b011d84f87c8ab8750ea86d9f6f
Jasonverycool <114458371+Jasonverycool@users.noreply.github.com>
2022-10-23 14:40:27 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -24,7 +24,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'od-Nv5wSpLpuBjSWgUa_fGtkM2NM',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '_U4tYMfqig02PjW1tpFc741YBwsR_sD6zMSvJJ5q-hc',
+      useTemplateId: '0001',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '12-04',
       festivals: [
@@ -52,7 +52,7 @@ const USER_CONFIG = {
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: 'yPwjkWqP0mp3haVXRh3PF6bqaVeHGRO-HQ_RkovzNIA',
+  CALLBACK_TEMPLATE_ID: '0002',
 
   CALLBACK_USERS: [
     {
